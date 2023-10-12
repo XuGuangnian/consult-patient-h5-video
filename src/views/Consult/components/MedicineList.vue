53cb113cc6f18dc73b3ee0cfaf6eb9a5a2290e2a
guqianfeng <fred_gqf0221@163.com>
2023-10-12 09:44:40 +0800

    van-list使用

--- src/views/Consult/components/MedicineList.vue
+++ src/views/Consult/components/MedicineList.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import MedicineCard from './MedicineCard.vue'
+const list = ref([])
+const loading = ref(false)
+const finished = ref(false)
+
+const onLoad = () => {
+  // 异步更新数据
+  // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+  setTimeout(() => {
+    for (let i = 0; i < 10; i++) {
+      list.value.push(list.value.length + 1)
+    }
+
+    // 加载状态结束
+    loading.value = false
+
+    // 数据全部加载完成
+    if (list.value.length >= 40) {
+      finished.value = true
+    }
+  }, 1000)
+}
 </script>
 
 <template>
   <div class="medicine-list">
-    <medicine-card v-for="i in 100" :key="i"></medicine-card>
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <medicine-card v-for="i in list" :key="i"></medicine-card>
+    </van-list>
   </div>
 </template>
 
